4893367a08f3bafd905b77d5760097d6983870d8
Anton <anton.bellantan@univ-lyon2.fr>
2025-06-20 13:24:12 +0200

    maj 1323

--- SAE_6_VCOD/site/page_3.py
+++ SAE_6_VCOD/site/page_3.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, session, redirect, url_for, flash, request
 from tools import get_all_years,get_regions,Elec, Gaz, Chauffage,IRIS, DEPARTMENTS, db
+from collections import defaultdict
+
 
 page3_bp = Blueprint('page3', __name__)
 
@@ -21,8 +23,6 @@ def page_3():
         model = Gaz
     elif conso_type == 'chauffage':
         model = Chauffage
-
-
     
     def get_repartition_operateur(model, year, region):
         query = db.session.query(model.operateur, db.func.sum(model.conso))\
@@ -38,7 +38,6 @@ def page_3():
         rows = query.all()
         return {op: round(conso) for op, conso in rows}
 
-
     def get_repartition_categorie(model, year, region):
         query = db.session.query(model.code_categorie_consommation, db.func.sum(model.conso))\
             .join(IRIS, IRIS.CODE_IRIS == model.iris)\
@@ -53,8 +52,41 @@ def page_3():
         rows = query.all()
         return {cat: round(conso) for cat, conso in rows}
 
+    def get_fournisseur_categorie(model, year, region=None):
+        query = db.session.query(
+            model.operateur,
+            model.code_categorie_consommation,
+            db.func.sum(model.conso)
+        ).filter(model.annee == year)
+
+        if region:
+            query = query.join(IRIS, IRIS.CODE_IRIS == model.iris).join(DEPARTMENTS, DEPARTMENTS.num_dep == IRIS.DEP)
+            query = query.filter(DEPARTMENTS.region_name == region)
+
+        query = query.group_by(model.operateur, model.code_categorie_consommation)
+        rows = query.all()
+
+        # Regrouper les consommations par opérateur + catégorie
+        data = defaultdict(lambda: {"RES": 0, "ENT": 0, "PRO": 0, "ENT_PRO": 0})
+        for op, cat, conso in rows:
+            if cat in data[op]:
+                data[op][cat] += conso
+
+        # Calculer total pour trier
+        fournisseurs_total = {
+            op: sum(cats.values())
+            for op, cats in data.items()
+        }
+
+        # Garder les top 5
+        top5 = sorted(fournisseurs_total.items(), key=lambda x: x[1], reverse=True)[:5]
+        top_data = {op: data[op] for op, _ in top5}
+
+        return top_data
+    
     operateur_repartition = get_repartition_operateur(model, year,region)
     categorie_repartition = get_repartition_categorie(model, year,region)
+    fournisseur_categorie = get_fournisseur_categorie(model, year, region)
 
     return render_template("page_3.html",
         annees=annees,
@@ -63,5 +95,6 @@ def page_3():
         year=year,
         conso_type=conso_type,
         operateur_repartition=operateur_repartition,
-        categorie_repartition=categorie_repartition
+        categorie_repartition=categorie_repartition,
+        fournisseur_categorie=fournisseur_categorie
     )
